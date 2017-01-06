beb1adcdc7b60cb3d2d00608b9b7afb62ab65c44
Vai van Ommeren <vaivanommeren@gmail.com>
2017-01-06 14:23:58 +0100

    Begin gemaakt.

---
+++ KillerAppSE2/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KillerAppSE2.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }   
+    }
+}
