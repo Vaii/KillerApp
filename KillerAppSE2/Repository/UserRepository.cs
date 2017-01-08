24c8658f3ca64f874de016f1e8ecf5f3cb17f35e
Vai van Ommeren <vaivanommeren@gmail.com>
2017-01-08 23:46:36 +0100

    Update documentatie

---
+++ KillerAppSE2/Repository/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KillerAppSE2.Interfaces;
+using KillerAppSE2.Models;
+
+namespace KillerAppSE2.Repository
+{
+    public class UserRepository
+    {
+        private IContextUser _iContextUser;
+        private Student student { get; set; }
+        private Ouder ouder { get; set; }
+
+        public UserRepository(IContextUser context)
+        {
+            this._iContextUser = context;
+        }
+
+        public bool UserRegister(Ouder User)
+        {
+            return _iContextUser.RegisterUser(User);
+        }
+
+        public bool UserRegister(Student User)
+        {
+            return _iContextUser.RegisterUser(User);
+        }
+
+        public Student LoginStudent(string email, string password)
+        {
+            student = _iContextUser.LoginStudent(email, password);
+
+            if (student != null)
+            {
+                return student;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Ouder LoginOuder(string email, string password)
+        {
+            ouder = _iContextUser.LoginOuder(email, password);
+            return ouder;
+        }
+    }
+}
---
+++ StumobielTests/BeschikbaarheidTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KillerAppSE2;
+using KillerAppSE2.Models;
+using KillerAppSE2.Repository;
+using KillerAppSE2.Context;
+using KillerAppSE2.Context.Student;
+
+namespace StumobielTests
+{
+    [TestClass]
+    public class BeschikbaarheidTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StudentRepository StuRep = new StudentRepository(new MemoryContextStudent());
+            Student TestStudent = new Student();
+            string beginTijd = "12:00";
+            string eindTijd = "15:00";
+            DateTime Dag = DateTime.Today;
+            bool verwachteResultaat = true;
+
+            bool resultaat = StuRep.RegistreerBeschikbaarheid(TestStudent, beginTijd, eindTijd, Dag);
+
+            Assert.AreEqual(verwachteResultaat, resultaat, "Het is niet gelukt");
+
+        }
+    }
+}
